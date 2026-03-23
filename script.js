3ef95d683a66825ffb6e5097bdfb3340e1191e17
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-23 15:16:48 +0530

    drag grow progress

--- script.js
+++ script.js
@@ -9,7 +9,19 @@ const done = document.querySelector("#done");
 const tasks = document.querySelectorAll('.task');
 
 tasks.forEach(task => {
-    task.addEventListener("drag", (e)=>{
-        console.log("dragging ", e);
+    task.addEventListener("drag", (e)=>{ // dragging element
+        // console.log("dragging ", e);
     })
 });
+
+// this effect is also used to add and remove highlights in elements in a website
+progress.addEventListener("dragenter", (e)=>{
+    // console.log("dragenter");
+    progress.classList.add("hover-over");
+    
+})
+
+progress.addEventListener("dragleave", (e)=>{
+    progress.classList.remove("hover-over");
+
+})
